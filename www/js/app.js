40e6cab8fd16b9c8b72fadf4c934a48fbdf27ab2
sharil1993 <sharilfazlee9348@gmail.com>
2015-06-29 09:19:31 +0800

    spek

--- www/js/app.js
+++ www/js/app.js
@@ -84,6 +84,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     .state('tab.f07', { url: '/f07', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f07.html'}}})
     .state('tab.f08', { url: '/f08', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f08.html'}}})
     .state('tab.f09', { url: '/f09', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f09.html'}}})
+    .state('tab.f10', { url: '/f10', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f10.html'}}})
     .state('tab.f13', { url: '/f13', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f13.html'}}})
     .state('tab.f14', { url: '/f14', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f14.html'}}})
     .state('tab.f15', { url: '/f15', views: { 'tab-firsttime': { templateUrl: 'templates/forms/f15.html'}}})
